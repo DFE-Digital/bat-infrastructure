2cc35eca60d54f67ef0ed1fa02a25719e3a32392
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-03-15 19:38:00 +0000

    App names for alert rules in separate variable

--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -22,8 +22,14 @@ variable influxdb_service_plan {}
 
 variable alertmanager_slack_url {}
 
+variable alertmanager_app_names {}
+
 locals {
   paas_api_url               = "https://api.london.cloud.service.gov.uk"
   alertmanager_slack_channel = "twd_bat_devops"
-  alert_rules                = templatefile("./config/alert.rules.tmpl", { apps = ["find-prod"] })
+  alert_rules_variables = {
+    grafana_dashboard_url = "https://grafana-bat.london.cloudapps.digital/d/eF19g4RZx/cf-apps?orgId=1&refresh=10s"
+    apps                  = var.alertmanager_app_names
+  }
+  alert_rules = templatefile("./config/alert.rules.tmpl", local.alert_rules_variables)
 }
--- monitoring/workspace-variables/prod.tfvars
+++ monitoring/workspace-variables/prod.tfvars
@@ -1,3 +1,4 @@
 monitoring_space_name    = "bat-prod"
 monitoring_instance_name = "bat"
 influxdb_service_plan    = "small-1_x"
+alertmanager_app_names   = ["find-prod", "publish-teacher-training-prod", "teacher-training-api-prod", "register-production"]
