4e093143270e7395129b8ce245f8517179214c62
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-05-20 11:15:46 +0100

    Update to grafana 7.2.2

--- monitoring/resources.tf
+++ monitoring/resources.tf
@@ -12,6 +12,7 @@ module prometheus_all {
   grafana_admin_password       = var.grafana_admin_password
   grafana_google_client_id     = var.grafana_google_client_id
   grafana_google_client_secret = var.grafana_google_client_secret
+  grafana_runtime_version      = "7.2.2"
 
   alert_rules            = local.alert_rules
   alertmanager_slack_url = var.alertmanager_slack_url
