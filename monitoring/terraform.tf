6c9b0895402bf8ef390f2261bc11f4f6451a0ad6
Vignesh.N <vignesh.natraj@digital.education.gov.uk>
2021-06-07 16:15:13 +0100

    Update providers

--- monitoring/terraform.tf
+++ monitoring/terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-  required_version = "~> 0.13.5"
+  required_version = "~> 0.15.5"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.59.0"
+      version = "2.62.0"
     }
     cloudfoundry = {
       source  = "cloudfoundry-community/cloudfoundry"
-      version = "0.14.1"
+      version = "0.14.2"
     }
   }
   backend "azurerm" {
