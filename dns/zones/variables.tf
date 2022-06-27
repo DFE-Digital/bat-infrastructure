39daa6952e33c45bff9b421e4dd3a0e6f75cbcdf
Colin Saliceti <colin.saliceti@digital.education.gov.uk>
2022-06-27 16:03:15 +0100

    Call dns/zones module in terraform-modules

--- dns/zones/variables.tf
+++ dns/zones/variables.tf
@@ -6,11 +6,12 @@ variable "hosted_zone" {
   default = {}
 }
 
-variable "environment" { default = "Prod" }
-variable "portfolio" { default = "Early Years and Schools Group" }
-variable "product" { default = "Find postgraduate teacher training" }
-variable "service" { default = "Teacher services" }
-
 locals {
   azure_credentials = try(jsondecode(var.azure_credentials), null)
+  tags = {
+    "Environment" = "Prod"
+    "Portfolio"   = "Early Years and Schools Group"
+    "Product"     = "Find postgraduate teacher training"
+    "Service"     = "Teacher services"
+  }
 }
