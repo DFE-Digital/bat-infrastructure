cb349b85b9d971410a4059a86d2da085a2f90aab
Stephen Neal <smneal@users.noreply.github.com>
2022-12-16 15:33:41 +0000

    Add backend and base tf files

---
+++ containerapp/terraform.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = "~> 1.3.6"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.36.0"
+    }
+
+    azapi = {
+      source = "Azure/azapi"
+      version = "1.1.0"
+    }
+  }
+  backend "azurerm" {
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  skip_provider_registration = true
+  subscription_id            = try(local.azure_credentials.subscriptionId, null)
+  client_id                  = try(local.azure_credentials.clientId, null)
+  client_secret              = try(local.azure_credentials.clientSecret, null)
+  tenant_id                  = try(local.azure_credentials.tenantId, null)
+}
+
+provider "azapi" {
+  # Configuration options
+}
---
+++ containerapp/workspace_variables/backend_qa.tfvars
@@ -0,0 +1,4 @@
+resource_group_name  = "s121d01-capoc-rg"
+storage_account_name = "s121d01capoc"
+container_name       = "capoc-tfstate"
+key                  = "capoc-terraform.tfstate"
