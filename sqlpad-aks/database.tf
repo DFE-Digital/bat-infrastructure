98d295b01a38ed1c9d8ad84e10411d1a6a0c8972
RMcVelia <rick.mcvelia@gmail.com>
2023-07-19 14:03:42 +0100

    Migrate sqlpad to aks

---
+++ sqlpad-aks/database.tf
@@ -0,0 +1,16 @@
+module "postgres" {
+  source = "./vendor/modules/aks//aks/postgres"
+
+  namespace             = var.namespace
+  environment           = var.environment
+  azure_resource_prefix = var.azure_resource_prefix
+  service_short         = var.service_short
+  config_short          = var.config_short
+  service_name          = local.service_name
+
+  cluster_configuration_map = module.cluster_data.configuration_map
+
+  use_azure               = var.deploy_azure_backing_services
+  azure_enable_monitoring = false
+  server_version          = "14"
+}
